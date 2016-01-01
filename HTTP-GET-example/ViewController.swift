f8b98624a72815d8f0c32788c778e163376b8757
Sergey Kargopolov <flylet@softlets.com>
2016-01-01 15:47:01 -0800

    Initial Commit

---
+++ HTTP-GET-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTTP-GET-example
+//
+//  Created by Sergey Kargopolov on 2016-01-01.
+//  Copyright © 2016 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
